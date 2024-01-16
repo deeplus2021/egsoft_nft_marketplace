bdda54d2319e9e9f2705e8366992f843abe1b637
biginfo2012 <biginfo2012@gmail.com>
2024-01-16 13:36:48 +0300

    Implement Profile Menu Components #26
    Added visual design elements, including color schemes, layout arrangements, and responsive adjustments to enhance the aesthetic appeal of the Profile Menu component. This commit completes task #26
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- src/app/component/navbar/index.tsx
+++ src/app/component/navbar/index.tsx
@@ -2,8 +2,13 @@
 "use client"
 import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
 import { useEffect } from 'react';
-import { MdOutlineWallet } from "react-icons/md";
 import { shortenWalletAddress } from '@/app/utils/Web3Modal';
+
+//icons
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import { MdOutlineWallet } from "react-icons/md";
+import ProfileMenu from '../menus/profile.menu';
+
 
 const Navbar = () => {
     const { open } = useWeb3Modal()
@@ -29,16 +34,19 @@ const Navbar = () => {
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             {
-                                isConnected ? 
-                                <button onClick={() => open()} className="flex items-center transition duration-200 ease-in-out bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded relative" style={{ borderRadius: '12px', padding: '0px 12px', height: '48px', minWidth: '48px', borderWidth: '1px', borderStyle: 'solid', borderColor: 'transparent', backgroundColor: 'rgba(255, 255, 255, 0.12)', color: 'rgb(255, 255, 255)', backdropFilter: 'blur(20px)' }}>
-                                    <MdOutlineWallet size={20} className="mr-2" />
-                                    <span>{shortenWalletAddress(address)}</span>
-                                </button> : 
-                                <button onClick={() => open()} className="flex items-center transition duration-200 ease-in-out bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded relative" style={{ borderRadius: '12px', padding: '0px 12px', height: '48px', minWidth: '48px', borderWidth: '1px', borderStyle: 'solid', borderColor: 'transparent', backgroundColor: 'rgba(255, 255, 255, 0.12)', color: 'rgb(255, 255, 255)', backdropFilter: 'blur(20px)' }}>
-                                    <MdOutlineWallet size={20} className="mr-2" />
-                                    <span>Connect</span>
-                                </button>
+                                isConnected ?
+                                    <button onClick={() => open()} className="flex items-center transition duration-200 ease-in-out bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded relative" style={{ borderRadius: '12px', padding: '0px 12px', height: '48px', minWidth: '48px', borderWidth: '1px', borderStyle: 'solid', borderColor: 'transparent', backgroundColor: 'rgba(255, 255, 255, 0.12)', color: 'rgb(255, 255, 255)', backdropFilter: 'blur(20px)' }}>
+                                        <MdOutlineWallet size={25} className="mr-2" />
+                                        <span>{shortenWalletAddress(address)}</span>
+                                    </button> :
+                                    <button onClick={() => open()} className="flex items-center transition duration-200 ease-in-out bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded relative" style={{ borderRadius: '12px', padding: '0px 12px', height: '48px', minWidth: '48px', borderWidth: '1px', borderStyle: 'solid', borderColor: 'transparent', backgroundColor: 'rgba(255, 255, 255, 0.12)', color: 'rgb(255, 255, 255)', backdropFilter: 'blur(20px)' }}>
+                                        <MdOutlineWallet size={25} className="mr-2" />
+                                        <span>Connect</span>
+                                    </button>
                             }
+                        </li>
+                        <li>
+                            <ProfileMenu />
                         </li>
                     </ul>
                 </div>
